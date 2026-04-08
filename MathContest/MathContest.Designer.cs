1e4477738ac9ed640c44f553397a59bb3ec8b8c9
lukeg <groeluke@isu.edu>
2026-04-07 22:17:37 -0600

    adding buttons and group box

--- MathContest/MathContest.Designer.cs
+++ MathContest/MathContest.Designer.cs
@@ -28,18 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 77);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(8, 113);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 77);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(8, 196);
+            button3.Name = "button3";
+            button3.Size = new Size(200, 77);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(588, 261);
+            button4.Name = "button4";
+            button4.Size = new Size(200, 77);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Location = new Point(233, 53);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(214, 397);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
             // 
             // MathContest
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(button4);
             Name = "MathContest";
             Text = "Math Contest";
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private GroupBox groupBox1;
     }
 }
--- MathContest/MathContest.cs
+++ MathContest/MathContest.cs
@@ -1,3 +1,11 @@
+/*Luke Groesbeck
+Spring 2026
+RCET 2265
+Project MathContest
+Computer Fundamentals and Introduction to Programming
+https://github.com/groeluke/MathContest.git
+*/
+
 namespace MathContest
 {
     public partial class MathContest : Form
